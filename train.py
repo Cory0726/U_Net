04773a77497ea2d29d83c2c903d18734f9b44ae5
Cory0726 <lkfu520@gmail.com>
2025-12-18 17:02:31 +0800

    update

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
 import os
+
+import cv2
 # import random
 # import sys
 import torch
@@ -173,12 +175,12 @@ def train_model(
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=50, help='Number of epochs')  # 5 or 10 or 20
-    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=8, help='Batch size')  # 32 or 16
+    parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=16, help='Batch size')  # 32 or 16
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
                         help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
-    parser.add_argument('--scale', '-s', type=float, default=1.0, help='Downscaling factor of the images')  # 0.5
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
+    parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')  # 0.5
+    parser.add_argument('--validation', '-v', dest='val', type=float, default=20.0,
                         help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')  # True
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
@@ -187,7 +189,7 @@ def get_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     args = get_args()
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -240,3 +242,7 @@ if __name__ == '__main__':
             val_percent=args.val / 100,
             amp=args.amp
         )
+
+if __name__ == '__main__':
+    img = cv2.imread('data/imgs/img_00001.png')
+    print(img.shape)
