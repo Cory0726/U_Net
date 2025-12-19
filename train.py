9724823f581c4dc77e761d7aeea814fe2edb6d70
Cory0726 <lkfu520@gmail.com>
2025-12-19 09:38:24 +0800

    update

--- train.py
+++ train.py
@@ -171,7 +171,6 @@ def train_model(
             torch.save(state_dict, str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch)))
             logging.info(f'Checkpoint {epoch} saved!')
 
-
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=50, help='Number of epochs')  # 5 or 10 or 20
@@ -187,7 +186,6 @@ def get_args():
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
 
     return parser.parse_args()
-
 
 def main():
     args = get_args()
